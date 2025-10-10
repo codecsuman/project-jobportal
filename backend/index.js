cde4337d43e8b2a31c7d7be73781e6464ee5affb
SUMAN JHANP <sumanjhanp1@gmail.com>
2025-10-10 22:41:02 +0530

    url change

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173", // change to process.env.FRONTEND_URL in production
+  origin: "https://project-jobportal-4.onrender.com", // change to process.env.FRONTEND_URL in production
   credentials: true,
 };
 app.use(cors(corsOptions));
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,8 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT = "https://project-jobportal-4.onrender.com/api/v1/user";
 import { Company } from "./../../../backend/models/company.model";
 
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const JOB_API_END_POINT = "https://project-jobportal-4.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
+  "https://project-jobportal-4.onrender.com/api/v1/application";
 
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+export const COMPANY_API_END_POINT = "https://project-jobportal-4.onrender.com/api/v1/company";
